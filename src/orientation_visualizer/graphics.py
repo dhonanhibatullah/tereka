8a001a6344b0416c1427202e241773e348f9fbd2
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2024-10-18 21:41:14 +0700

    Fix: car switched to husky

--- src/orientation_visualizer/graphics.py
+++ src/orientation_visualizer/graphics.py
@@ -1,22 +1,20 @@
 import os
-import time
 import pybullet as pb
 import pybullet_data as pbdata
-import numpy as np
 
 
 class GraphicsController:
 
     def __init__(self) -> None:
         self.PLANE_URDF_PATH    = os.path.join(pbdata.getDataPath(), 'plane.urdf')
-        self.TRUCK_URDF_PATH    = os.path.join(pbdata.getDataPath(), 'r2d2.urdf')
+        self.CAR_URDF_PATH      = os.path.join(pbdata.getDataPath(), 'husky/husky.urdf')
         self.UPDATE_PERIOD      = 1./60.
         self.PHYSICS_CLIENT     = pb.connect(pb.GUI)
         
         pb.loadURDF(self.PLANE_URDF_PATH)
         pb.setGravity(0.0, 0.0, 0.0)
-        self.truck_box_id = pb.loadURDF(
-            self.TRUCK_URDF_PATH,
+        self.car_box_id = pb.loadURDF(
+            self.CAR_URDF_PATH,
             [0, 0, 1],
             pb.getQuaternionFromEuler([0, 0, 0])
         )
@@ -26,7 +24,7 @@ class GraphicsController:
 
     def setRotation(self, quaternion: list[float]) -> None:
         pb.resetBasePositionAndOrientation(
-            self.truck_box_id,
+            self.car_box_id,
             [0, 0, 1],
             quaternion
         )
--- src/orientation_visualizer/main.py
+++ src/orientation_visualizer/main.py
@@ -4,17 +4,17 @@ from .graphics import GraphicsController
 
 def main(args=None):
     print('[orientation_visualizer] Started!')
-    serial_controller   = SerialController()
+    # serial_controller   = SerialController()
     graphics_controller = GraphicsController()
 
     try:
         while True:
-            quaternion = serial_controller.readData()
-            graphics_controller.setRotation(quaternion)
+            # quaternion = serial_controller.readData()
+            # graphics_controller.setRotation(quaternion)
             graphics_controller.step()
 
     except KeyboardInterrupt:
-        serial_controller.stop()
+        # serial_controller.stop()
         graphics_controller.stop()
         print('[orientation_visualizer] Stopped!')
         return
