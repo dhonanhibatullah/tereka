b0863b9ed244f99d181a766b6e3d9893d6abbc98
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2024-10-26 22:05:07 +0700

    Dev: Filter enabled

--- mcu/lib/Tereka/tereka.cpp
+++ mcu/lib/Tereka/tereka.cpp
@@ -46,10 +46,10 @@ void tereka::update_q_filt() {
 
 
 void tereka::transmit_data() {
-    // for(uint8_t i = 0; i < 4; ++i) {
-    //     Serial.print(String(tereka::q_raw[i], 5));
-    //     Serial.print(',');
-    // }
+    for(uint8_t i = 0; i < 4; ++i) {
+        Serial.print(String(tereka::q_raw[i], 5));
+        Serial.print(',');
+    }
     for(uint8_t i = 0; i < 4; ++i) {
         Serial.print(String(tereka::q_filt[i], 5));
         Serial.print(',');
